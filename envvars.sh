4b8d780257f19e25755fc7a3cd52afad7c2115f3
James Knott <k8sclass2018@gmail.com>
2018-10-27 11:09:19 -0500

    First Commit

---
+++ envvars.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+            export DOMAIN_NAME="aheadlabs.io"
+            export CLUSTER_ALIAS="prod"
+            export CLUSTER_FULL_NAME="${CLUSTER_ALIAS}.${DOMAIN_NAME}"
+            export CLUSTER_AWS_AZ="us-west-2a"
+            export KOPS_STATE_STORE="s3://${CLUSTER_FULL_NAME}-state"
+            export AWS_ACCESS_KEY_ID=`aws configure get aws_access_key_id`
+            export AWS_SECRET_ACCESS_KEY=`aws configure get aws_secret_access_key`
+            # Optional export DOMAIN_NAME_ZONE_ID=Z1VROASDFFASDFAS
