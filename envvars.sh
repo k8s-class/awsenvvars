4a3358edcb977d4026b8a5e6610c4937b9f80a03
James Knott <k8sclass2018@gmail.com>
2018-10-27 11:11:24 -0500

    Second Commit - Added create bucket command and full clustername state

--- envvars.sh
+++ envvars.sh
@@ -9,3 +9,7 @@
             export AWS_ACCESS_KEY_ID=`aws configure get aws_access_key_id`
             export AWS_SECRET_ACCESS_KEY=`aws configure get aws_secret_access_key`
             # Optional export DOMAIN_NAME_ZONE_ID=Z1VROASDFFASDFAS
+
+            aws s3api create-bucket --bucket ${CLUSTER_FULL_NAME}-state
+
+            export KOPS_STATE_STORE="s3://${CLUSTER_FULL_NAME}-state"
